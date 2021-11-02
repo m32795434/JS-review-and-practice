f0a16a355562eec37677dedf2005ab5022e120ff
m32795434 <manubravard@gmail.com>
2021-11-02 00:20:18 -0300

    click outside and card translate!

--- exersices/34 - Click Outside/click-outside.js
+++ exersices/34 - Click Outside/click-outside.js
@@ -1,14 +1,39 @@
 const cardButtons = document.querySelectorAll('.card button');
-
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner');
 function handleCardButtonClick(event) {
   const button = event.currentTarget;
   const card = button.closest('.card');
   // grab the img source
   const imgSrc = card.querySelector('img').src;
   const desc = card.dataset.description;
-  console.log(desc);
+  const name = card.querySelector('h2').textContent;
+  // populate the modal with the new info
+  modalInner.innerHTML = `
+<img  width="400" height="400" src="${imgSrc.replace(
+    '200',
+    '400'
+  )}" alt="${name}"/>
+<p>${desc}</p>
+`;
+  // show the modal
+  modalOuter.classList.add('open');
 }
-
 cardButtons.forEach((button) =>
   button.addEventListener('click', handleCardButtonClick)
 );
+function closeModal() {
+  modalOuter.classList.remove('open');
+}
+
+modalOuter.addEventListener('click', (event) => {
+  const isOutside = !event.target.closest('.modal-inner');
+  if (isOutside) {
+    closeModal();
+  }
+});
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
