5efe6996cc70f948879e78b2806b904382757a7c
Manuel Augusto Bravard <manubravard@gmail.com>
2022-04-25 20:23:49 -0300

    Async Typer ready!

--- exersices/73 - Async Typer/scripts.js
+++ exersices/73 - Async Typer/scripts.js
@@ -16,6 +16,7 @@ function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
   return Math.floor(randomNumber * (max - min) + min);
 }
 // FIRST WITH AN ASYNC FOR OF LOOP
+
 async function draw(el) {
   const text = el.textContent;
   let soFar = '';
@@ -27,4 +28,19 @@ async function draw(el) {
   }
 }
 // SECOND WITH RECURSION
+/*
+function draw(el) {
+  const text = el.textContent;
+  let index = 1;
+  const { typeMin, typeMax } = el.dataset;
+  async function drawLetter() {
+    el.textContent = text.slice(0, index);
+    index += 1;
+    if (index <= text.length) {
+      await wait(getRandomBetween(typeMin, typeMax));
+      drawLetter();
+    }
+  }
+  drawLetter();
+} */
 document.querySelectorAll('[data-type]').forEach(draw);
