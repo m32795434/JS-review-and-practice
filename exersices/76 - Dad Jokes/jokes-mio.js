8a539b6f8abb68c4b0f419b078cd402d009f2f53
Manuel Augusto Bravard <manubravard@gmail.com>
2022-05-05 01:26:27 -0300

    fetching and sticking a css loader

--- exersices/76 - Dad Jokes/jokes-mio.js
+++ exersices/76 - Dad Jokes/jokes-mio.js
@@ -1,5 +1,6 @@
 const jokeButton = document.querySelector('.getJoke');
 const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -11,16 +12,28 @@ const buttonText = [
   'that was the worst one',
 ];
 async function fetchJoke() {
+  loader.style.display = 'inline-block';
   const response = await fetch('https://icanhazdadjoke.com/', {
     headers: { Accept: 'application/json' },
   });
   const data = await response.json();
   return data;
 }
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  if (item === not) {
+    console.log(`repeted item!:${not}`);
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
 async function handleClick() {
   const { joke } = await fetchJoke();
+  loader.style.display = 'none';
   jokeHolder.textContent = joke;
-  console.log(joke);
+  jokeButton.textContent = randomItemFromArray(
+    buttonText,
+    jokeButton.textContent
+  );
 }
 jokeButton.addEventListener('click', handleClick);
-fetchJoke();
