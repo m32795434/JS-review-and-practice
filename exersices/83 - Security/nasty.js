ef2802ae07902dd53d9702fbe3c2d191dd9ef81e
Manuel Augusto Bravard <manubravard@gmail.com>
2022-05-18 09:41:39 -0300

    dompurify, FORBID_ATTR, FORBID_TAGS

--- exersices/83 - Security/nasty.js
+++ exersices/83 - Security/nasty.js
@@ -1,17 +1,23 @@
 import { sanitize } from 'dompurify';
 
+console.log('horray!');
+
 const input = document.querySelector('[name="input"]');
 const output = document.querySelector('.output');
 const buttons = document.querySelectorAll('nav button');
 input.addEventListener('input', () => {
-  output.innerHTML = input.value.replace(/\n/g, '<br>');
+  const clean = sanitize(input.value, {
+    FORBID_ATTR: ['width', 'height', 'style', 'href', 'target'],
+    FORBID_TAGS: ['style', 'script'],
+  });
+  output.innerHTML = clean.replace(/\n/g, '<br>');
 });
 
 // trigger an input even on page load
 input.dispatchEvent(new Event('input'));
 
-buttons.forEach(button =>
-  button.addEventListener('click', e => {
+buttons.forEach((button) =>
+  button.addEventListener('click', (e) => {
     alert(e.currentTarget.textContent);
   })
 );
