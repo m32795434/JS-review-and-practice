ea0d210a8b87e95d7cabee89a464fc9b7cd3aadd
Manuel Augusto Bravard <manubravard@gmail.com>
2022-09-08 23:19:16 -0300

    notes

--- exersices/57 - Shopping List/shopping.js
+++ exersices/57 - Shopping List/shopping.js
@@ -13,21 +13,14 @@ function handleSubmit(e) {
     id: Date.now(),
     complete: false,
   };
-  // push the item into our state
   items.push(item);
-  console.log(`there are now ${items.length} items in the list`);
-
-  // clear the input
   //   e.currentTarget.item.value = '';
-  // clear all the inputs of our form
-  e.target.reset();
-
+  e.currentTarget.reset();
   // fire off a custom event that will tell anyone else who cares that the items have been updated!
   list.dispatchEvent(new CustomEvent('itemsUpdated'));
 }
 
 function displayItems() {
-  console.log(items);
   /* eslint-disable*/
   const html = items.map(item => `<li class="shopping-item">
   <input 
@@ -44,32 +37,32 @@ function displayItems() {
 
   list.innerHTML = html;
 }
+
 function mirrorToLocalStorage() {
-  console.log('saving items');
+  console.log('mirroringggg...');
   localStorage.setItem('items', JSON.stringify(items));
 }
+
 function restoreFromLocalStorage() {
   console.log('restoring from local storage');
-  // pull the items from local storage
   const lsItems = JSON.parse(localStorage.getItem('items'));
   if (lsItems.length) {
     items.push(...lsItems);
     /* another way to do this:
     1) items = lsItems
     2) lsItems.forEach(item => items.push(item)); */
-    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+    displayItems();
   }
 }
+
 function deleteItem(id) {
-  console.log(`deleting item with id ${id}`);
   items = items.filter((item) => item.id !== id);
   list.dispatchEvent(new CustomEvent('itemsUpdated'));
 }
 function markAsComplete(id) {
-  console.log('marking as complete', id);
   const itemRef = items.find((item) => item.id === id);
   itemRef.complete = !itemRef.complete;
-  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+  mirrorToLocalStorage();
 }
 
 shoppingForm.addEventListener('submit', handleSubmit);
