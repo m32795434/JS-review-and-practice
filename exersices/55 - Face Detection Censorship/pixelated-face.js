75d58aa76a6e208cc0b163fc75ff33472ea41fc9
Manuel Augusto Bravard <manubravard@gmail.com>
2023-02-21 14:03:13 -0300

    catching erros

--- exersices/55 - Face Detection Censorship/pixelated-face.js
+++ exersices/55 - Face Detection Censorship/pixelated-face.js
@@ -10,46 +10,49 @@ const faceCtx = faceCanvas.getContext('2d');
 const optionsInputs = document.querySelectorAll(
   '.controls input[type="range"]'
 );
+const options = {
+  SIZE: 10,
+  SCALE: 1.35,
+};
 function handleOption(event) {
   const { name, value } = event.currentTarget; // the NAME and the VALUE of the input
-  console.log(name, value);
+  // console.log(name, value);
   options[name] = parseFloat(value);
 }
 optionsInputs.forEach((input) => {
   input.addEventListener('input', handleOption);
 });
-
-const options = {
-  SIZE: 10,
-  SCALE: 1.35,
-};
-
-const faceDetector = new window.FaceDetector();
-console.log(video, canvas, faceCanvas, faceDetector);
+let faceDetector;
+try {
+  faceDetector = new window.FaceDetector();
+} catch (err) {
+  alert(
+    'You have to enable the Experimental Web Platform features. Go to "yourBrowser://flags" and enable!'
+  );
+}
+// console.log(video, canvas, faceCanvas, faceDetector);
 
 // write a function that will populate the users video
 
 async function populateVideo() {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: { width: 640, height: 360 },
-  });
+  let stream;
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({
+      video: { width: 640, height: 360 },
+    });
+  } catch (err) {
+    alert('You have to authorize the camera recording to use this app😄');
+  }
   video.srcObject = stream;
   await video.play();
   // size the canvases to be the same size as the video
-  console.log(video.videoWidth, video.videoHeight);
+  // console.log(video.videoWidth, video.videoHeight);
   canvas.height = video.videoHeight;
   canvas.width = video.videoWidth;
   faceCanvas.height = video.videoHeight;
   faceCanvas.width = video.videoWidth;
 }
 
-async function detect() {
-  const faces = await faceDetector.detect(video); // this "detect" is a method from the faceDetector object, and throws an array of faces.
-  faces.forEach(drawFace);
-  faces.forEach(censor);
-  // ask the browser when the next animation frame is, and tell it to run "detect" for us.
-  requestAnimationFrame(detect); // this performs better, but it could be just "detect();"
-}
 function drawFace(face) {
   const { width, height, top, left } = face.boundingBox;
   // console.log(face);
@@ -59,7 +62,6 @@ function drawFace(face) {
   ctx.lineWidth = 2;
   ctx.strokeRect(left, top, width, height); // paint the rectangle
 }
-
 function censor({ boundingBox: face }) {
   // faceCtx.imageSmoothingEnabled = false;
   faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
@@ -93,6 +95,13 @@ function censor({ boundingBox: face }) {
     width,
     height
   );
+}
+async function detect() {
+  const faces = await faceDetector.detect(video); // this "detect" is a method from the faceDetector object, and throws an array of faces.
+  faces.forEach(drawFace);
+  faces.forEach(censor);
+  // ask the browser when the next animation frame is, and tell it to run "detect" for us.
+  requestAnimationFrame(detect); // this performs better, but it could be just "detect();"
 }
 
 // if we don't have access to the globals variables. We can:
